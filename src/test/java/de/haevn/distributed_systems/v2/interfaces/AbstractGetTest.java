f3ded7a39e1c5a1ee91ac9928e3f7c3b48abed17
haevn <nils.milewski@stud.hs-ruhrwest.de>
2021-12-01 12:55:04 +0100

    Added documentation

--- src/test/java/de/haevn/distributed_systems/v2/interfaces/AbstractGetTest.java
+++ src/test/java/de/haevn/distributed_systems/v2/interfaces/AbstractGetTest.java
@@ -1,15 +1,6 @@
 package de.haevn.distributed_systems.v2.interfaces;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-public abstract class AbstractGetTest<T>{
-    protected final List<T> data = new ArrayList<>();
-    protected Optional<T> optionalObject;
-    protected Optional<T> emptyObject;
-    protected Optional<ArrayList<T>> emptyObjectList;
-
+public abstract class AbstractGetTest<T> extends AbstractTestData<T> {
     public abstract void get();
     public abstract void getById();
     public abstract void getByIdObjectDoesNotExists();
